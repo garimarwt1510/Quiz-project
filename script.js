290c10afd396b5f94309839d670965f03ee3233f
garimarwt1510 <garimarwt.2412@gmail.com>
2026-09-08 14:19:16 +0530

    Refactor quiz game logic and add replay functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let btns= document.querySelectorAll(".btn");
 let msg=document.querySelector(".msg");
 let startBtn=document.querySelector(".start_btn");
-let timer=document.querySelector("#timer");
+let time=document.querySelector("#timer");
 let startPg =document.querySelector(".start_pg");
 let popMsg=document.querySelector(".popup");
 let exit=document.querySelector("#exit");
@@ -10,6 +10,7 @@ let cnt=document.querySelector("#continue");
 let ques=document.querySelector("#Question");
 let score_msg=document.querySelector("#score");
 let next=document.querySelector("#next");
+let replay=document.querySelector("#replay");
 let currentQ=0;
 let currentO=0;
 let sec=0;
@@ -19,7 +20,7 @@ let score=0;
 
 
 startBtn.classList.remove("hide");
-startPg.classList.remove("hide");
+ startPg.classList.remove("hide");
  
 //quiz question  and option
 const quizData = [
@@ -50,66 +51,6 @@ const quizData = [
     }
 ];
 
-let storeOp=()=>{
-    if(currentQ < quizData.length){
-
-for(let btn of btns){
-    btn.innerText=quizData[currentQ].options[currentO];
-    currentO ++;
-}
-}
-}
-function game(){
-for(let btn of btns){
-    btn.addEventListener('click',(e)=>{
-        console.log(currentQ);
-        let select=e.target.innerText;
-        let ans=quizData[currentQ].answer;
-        clearInterval(id);
-
-
-        if(select==ans){
-            e.target.style.backgroundColor="#87e06c";
-            e.target.style.border="3px solid  #192a4a";
-              score++;
-        } 
-          else{
-            e.target.style.backgroundColor="#f34f4f";
-          e.target.style.color="white";
-            for(let i=0;i<4;i++){
-                if(btns[i].innerText==ans){
-                    btns[i].style.backgroundColor="#87e06c";
-                  
-                }
-            }
-          }
-
-            setTimeout(()=>{
-                e.target.style.backgroundColor="white";
-                e.target.style.color="#192a4a";
-                for(let btn of btns){
-                    btn.style.backgroundColor="white";
-                    btn.style.color="#192a4a";
-                }
-                if(currentQ==4){
-                    msg.classList.remove("hide");
-                    score_msg.innerText=`Nice, you got ${score} out of 5`;
-    
-                }
-    
-                currentQ++;
-                quest();
-                currentO=0;
-                storeOp();
-               
-            },1000);
-            
-        
-        });
-
-}
-}
-  
 
 //start btn
 startBtn.addEventListener('click', ()=>{
@@ -118,32 +59,95 @@ startBtn.addEventListener('click', ()=>{
     
 });
 
+//continue btn
 cnt.addEventListener('click',()=>{
     startPg.classList.add("hide");
-   
-    game();
+    timer();
+    store();
+});
+
+//question and option store
+let store=()=>{
+    if(currentQ < quizData.length){
+        ques.innerText= quizData[currentQ].question;
+        console.log(currentO,currentQ);
+
+for(let btn of btns){
+    btn.innerText=quizData[currentQ].options[currentO];
+    currentO ++;
+}
+
+}
+else{
+    clearInterval(id);
+    msg.classList.remove("hide");
+    score_msg.innerText=`Nice, you got ${score} out of 5`;
+      }
+   }
+
+//game function
+function game(){
+for(let btn of btns){
+    btn.addEventListener('click',(e)=>{
+        if(currentQ < quizData.length){
+            let select=e.target.innerText;
+            let ans=quizData[currentQ].answer;
+
+        if(select==ans){
+            e.target.style.backgroundColor="#91db6f";
+            e.target.style.border="3px solid  #192a4a";
+              score++;
+        } 
+          else{
+            e.target.style.backgroundColor="#f34f4f";
+          e.target.style.color="white";
+            for(let i=0;i<4;i++){
+                if(btns[i].innerText==ans){
+                    btns[i].style.backgroundColor="#91db6f";
+                  
+                }
+            }
+        }
+         setTimeout(()=>{
+                e.target.style.backgroundColor="white";
+                e.target.style.color="#192a4a";
+                for(let btn of btns){
+                    btn.style.backgroundColor="white";
+                    btn.style.color="#192a4a";
+                }
+                currentO=0;
+                currentQ++;
+                store();
+               
+            },1000);
+           }
+        });
+    }
+}
+
+game();
+
+//time
+function timer(){
     id= setInterval(()=>{
         msec++;
         if(msec==100){
             msec=0;
             sec++;
-            if(sec == 20)
+            if(sec == 60)
         {
             clearInterval(id);
+            score.innerText="Time is Over!!";
+            msg.classList.remove("hide");
         }    
-        }
+      }
         let MSec=msec<10 ? `0${msec}` :msec;
         let Sec=sec<10 ? `0${sec}` :sec;
-
-    timer.innerText=`${Sec}:${MSec}`;
+        time.innerText=`${Sec}:${MSec}`;
     
-    },10)
-    quest();
-    storeOp();
-
-   
-
-});
+    },10);
+ 
+}
 
 exit.addEventListener('click',()=>{
     startBtn.classList.toggle("hide");
@@ -152,19 +156,32 @@ exit.addEventListener('click',()=>{
 });
 
 exit1.addEventListener('click',()=>{
-    startBtn.classList.toggle("hide");
+    clearInterval(id);
+    startBtn.classList.remove("hide");
     popMsg.classList.toggle("hide");
-    msg.classList.add("hide");
-    startPg.classList.toggle("hide");
+    msg.classList.toggle("hide");
+    startPg.classList.remove("hide");
+    msec=0;  
+    sec=0;
+    currentO=0;
+    currentQ=0;
+    score=0;
+ 
+});
 
-    
-
+replay.addEventListener("click",()=>{
+    clearInterval(id);
+    msg.classList.toggle("hide");
+    startPg.classList.add("hide");
+    msec=0;  
+    sec=0;
+    currentO=0;
+    currentQ=0;
+    score=0;
+    timer();
+    store();
 });
 
 
-let quest=()=>{
-    if(currentQ < quizData.length){
-ques.innerText= quizData[currentQ].question;
-}
-}
+
 
